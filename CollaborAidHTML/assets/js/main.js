c38f45630c0cda0eca7bf2cc3c7a573bd848ed26
Oumastrooi <e_g172@txstate.edu>
2016-10-27 21:54:55 -0500

    changed some things
    combined html files into one folder

--- CollaborAidHTML/assets/js/main.js
+++ CollaborAidHTML/assets/js/main.js
@@ -4,6 +4,10 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+$('.message a').click(function(){
+   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
+
 (function($) {
 
 	skel.breakpoints({
