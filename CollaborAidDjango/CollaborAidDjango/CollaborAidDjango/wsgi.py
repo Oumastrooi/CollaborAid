e5740374555cdc386c4d35be09e8f9e02015ac0b
dsg49 <dsg49@txstate.edu>
2016-10-27 19:53:05 -0500

    restructured and renamed things

--- CollaborAidDjango/CollaborAidDjango/CollaborAidDjango/wsgi.py
+++ CollaborAidDjango/CollaborAidDjango/CollaborAidDjango/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for CollaborAidSite project.
+WSGI config for CollaborAidDjango project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CollaborAidSite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CollaborAidDjango.settings")
 
 application = get_wsgi_application()
