afbb725faf95d61412a1257b6042606410702f8d
tnhodges70 <tnh34@txstate.edu>
2016-11-15 13:50:56 -0800

    admin site working
    localhost:8000/admin/ working
    only able to edit superusers and groups

--- collabsite/website/urls.py
+++ collabsite/website/urls.py
@@ -2,7 +2,9 @@
 # website/urls.py
 from django.conf.urls import url
 from . import views
+from django.contrib import admin
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+	#url(r'^admin/', include(admin.site.urls)),
 ]
