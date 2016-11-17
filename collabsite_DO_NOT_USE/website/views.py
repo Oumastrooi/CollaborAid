3d5553eb36f99197924dc6d83d3ee4d2b184b8c6
dsg49 <dsg49@txstate.edu>
2016-11-17 14:13:11 -0600

    removing dead files

--- collabsite_DO_NOT_USE/website/views.py
+++
@@ -1,9 +0,0 @@
-#!python
-#website/views.py
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-# Create your views here.
-@login_required(login_url="login/")
-def home(request):
-	return render(request,"home.html")
